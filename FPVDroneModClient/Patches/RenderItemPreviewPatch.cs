30b83b0644b14356302d86f6dacfa289cf59ee97
peinwastaken <legokid173@gmail.com>
2026-04-01 17:14:46 +0300

    fix incorrect thumbnail generation for slotvisibilitytoggle items

---
+++ FPVDroneModClient/Patches/RenderItemPreviewPatch.cs
@@ -0,0 +1,52 @@
+using EFT.InventoryLogic;
+using FPVDroneModClient.Components;
+using FPVDroneModClient.Components.Base;
+using FPVDroneModClient.Helpers;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace FPVDroneModClient.Patches
+{
+    public class RenderItemPreviewPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            Type generic = typeof(GClass924<,>).MakeGenericType(typeof(Item), typeof(GInterface39));
+            return AccessTools.Method(generic, "method_4");
+        }
+
+        [PatchPrefix]
+        private static void PatchPrefix(GameObject model)
+        {
+            try
+            {
+                BaseDroneController droneController = model.GetComponentInChildren<BaseDroneController>();
+                SlotVisibilityToggler[] togglers = model.GetComponentsInChildren<SlotVisibilityToggler>();
+
+                if (droneController)
+                {
+                    for (int i = 0; i < togglers.Length; i++)
+                    {
+                        SlotVisibilityToggler toggler = togglers[i];
+                        toggler?.OnEquip();
+                    }
+                }
+                else
+                {
+                    if (togglers.Length > 0 && togglers[0] != null)
+                    {
+                        togglers[0].OnUnequip();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                DebugLogger.LogError(e.Message);
+                throw;
+            }
+        }
+    }
+}
